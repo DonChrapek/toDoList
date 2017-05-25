8add531c02dceb95c32e98cf5e397189ad99abfd
DonChrapek <daniel.chrapczynski@gmail.com>
2017-05-25 18:39:56 +0200

    Made 'ToDoService' and 'save' function

--- js/app.js
+++ js/app.js
@@ -17,15 +17,37 @@ app.config(function($routeProvider){
 })
 
 
-app.controller('HomeController', ['$scope', function($scope){
-    $scope.appTitle = 'To do list';
-}]);
-
-app.controller('ListController', ['$scope', '$location', function($scope, $location){
-    $scope.tasks = [
+app.service('ToDoService', function() {
+    
+    var toDoService = {};
+    
+    toDoService.tasks = [
         { id: 1, completed: true, taskName: 'Learn AJAX', date: '2017-05-24' },
         { id: 2, completed: true, taskName: 'Learn JSON', date: '2017-05-24' },
         { id: 3, completed: true, taskName: 'Learn RESTful API', date: '2017-05-24' }
     ];
+    
+    toDoService.save = function(entry) {
+        toDoService.tasks.push(entry);
+    };
+    
+    return toDoService;
+    
+});
+
+
+app.controller('HomeController', ['$scope', function($scope){
+    $scope.appTitle = 'To do list';
+}]);
+
+app.controller('ListController', ['$scope', '$location', 'ToDoService', function($scope, $location, ToDoService ){
+    $scope.tasks = ToDoService.tasks;
+    
+    $scope.task = {};
+    
+    $scope.save = function() {
+        ToDoService.save($scope.task);
+        $location.path('/');
+    };
     
 }]);
